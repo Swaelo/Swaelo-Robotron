3614473598b10d0120c54938f694ff4b81c27c51
Swaelo <harleylaurie@rocketmail.com>
2020-03-04 07:35:21 +1000

    player can now shoot projectiles

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,39 @@
+﻿// ================================================================================================================================
+// File:        PlayerShooting.cs
+// Description:	Allows the player to fire projectiles by clicking on the game window
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject PlayerProjectilePrefab;
+
+    private void Update()
+    {
+        //Shoot a projectile whenever the player clicks on the game window
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Shoot a ray through the camera to see where the players mouse cursor is positioned
+            Ray CursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit RayHit;
+            if(Physics.Raycast(CursorRay, out RayHit))
+            {
+                //Find the direction from the player to where the mouse cursor is
+                Vector3 MouseDirection = Vector3.Normalize(RayHit.point - transform.position);
+
+                //Get the angle of this vector so that same angle can be applied to the projectile when we spawn it in
+                float ShotAngle = Vector3.Angle(MouseDirection, transform.right);
+
+                //Create a quaternion to apply to the projectile
+                bool ShotAbove = RayHit.point.y >= transform.position.y;
+                Quaternion ShotRotation = Quaternion.Euler(0f, 0f, ShotAbove ? ShotAngle : -ShotAngle);
+
+                //Spawn the projectile and give it some velocity
+                GameObject Projectile = Instantiate(PlayerProjectilePrefab, transform.position + (MouseDirection*.25f), ShotRotation);
+                Projectile.GetComponent<ProjectileMovement>().SetMovement(MouseDirection);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ProjectileMovement.cs
@@ -0,0 +1,36 @@
+﻿// ================================================================================================================================
+// File:        ProjectileMovement.cs
+// Description:	Applys movement to the projectile every frame
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    public float MoveSpeed = 3.5f;
+    private Vector3 MovementDirection;
+    private bool DirectionSet = false;
+
+    public void SetMovement(Vector3 Movement)
+    {
+        DirectionSet = true;
+        MovementDirection = Movement;
+    }
+
+    private void Update()
+    {
+        if (DirectionSet)
+            transform.position += MovementDirection * MoveSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Simply delete the projectile when it hits on of the border walls
+        if(collision.transform.CompareTag("Wall"))
+        {
+            GameObject.Destroy(this.gameObject);
+            return;
+        }
+    }
+}
