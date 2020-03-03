6379d49675a5b535bd8641d6a485f21c54dcbc23
Swaelo <harleylaurie@rocketmail.com>
2020-03-04 06:15:51 +1000

    Player character setup with basic movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿// ================================================================================================================================
+// File:        PlayerMovement.cs
+// Description:	Allows the player to move around the screen
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 1.5f;
+
+    private void Update()
+    {
+        //Create a new movement vector based on users keyboard input
+        Vector3 MovementVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        //Use the movement vector to update the players position
+        transform.position += MovementVector * MoveSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpriteViewer.cs
@@ -0,0 +1,59 @@
+﻿// ================================================================================================================================
+// File:        PlayerSpriteViewer.cs
+// Description:	Handles which of the player sprites are shown depending on which direction the player has been moving
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using UnityEngine;
+
+public class PlayerSpriteViewer : MonoBehaviour
+{
+    //Sprites for the front and side views of the player
+    public GameObject FrontSprites;
+    public GameObject SideSprites;
+
+    //Animators controlling the walking animations
+    public Animator FrontViewAnimator;
+    public Animator SideViewAnimator;
+
+    //Renderers for Side Body components, so they can be flipped when moving left/right
+    public SpriteRenderer SideBodyRenderer;
+    public SpriteRenderer SideEyesRenderer;
+
+    //Players change in position over time is used to determine which sprites need to be displayed and when to play the animations
+    private Vector3 PreviousPosition;
+
+    private void Awake()
+    {
+        PreviousPosition = transform.position;
+        SideSprites.SetActive(false);
+    }
+
+    private void Update()
+    {
+        //Find distance travelled in each direction this frame
+        float VerticalMovement = Mathf.Abs(transform.position.x - PreviousPosition.x);
+        float HorizontalMovement = Mathf.Abs(transform.position.y - PreviousPosition.y);
+
+        //Tell the animators if we are currently moving
+        bool IsMoving = transform.position != PreviousPosition;
+        FrontViewAnimator.SetBool("IsMoving", IsMoving);
+        SideViewAnimator.SetBool("IsMoving", IsMoving);
+
+        if(IsMoving)
+        {
+            //Update what sprites are being displayed if movement has occured
+            bool MovingVertical = VerticalMovement < HorizontalMovement;
+            FrontSprites.SetActive(MovingVertical);
+            SideSprites.SetActive(!MovingVertical);
+
+            //Flip the side view sprites based on left/right movement
+            bool MovingLeft = transform.position.x < PreviousPosition.x;
+            SideBodyRenderer.flipX = !MovingLeft;
+            SideEyesRenderer.flipX = !MovingLeft;
+        }
+
+        //Store current position for next frames comparison
+        PreviousPosition = transform.position;
+    }
+}
