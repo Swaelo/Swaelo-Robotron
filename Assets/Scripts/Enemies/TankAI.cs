0bdbc1bde30053a2ad466e7750a1af06d071ff01
Swaelo <harleylaurie@rocketmail.com>
2020-03-28 17:28:26 +1000

    The Tanks rebounding shots have been implemented

--- Assets/Scripts/Enemies/TankAI.cs
+++ Assets/Scripts/Enemies/TankAI.cs
@@ -7,6 +7,15 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+//Tanks will target one of the walls and attempt to bounce their shot off the wall to hit the player
+public enum WallTarget
+{
+    TopWall = 0,
+    BottomWall = 1,
+    LeftWall = 2,
+    RightWall = 3
+}
+
 public class TankAI : HostileEntity
 {
     //Movement/Targetting
@@ -99,7 +108,108 @@ public class TankAI : HostileEntity
                 //Tell the shot its initial movement direction
                 TankShell.GetComponent<TankShellAI>().InitializeProjectile(ShotDirection, this);
             }
+            //The non direct shots will aim to hit the player after bouncing off one of the walls
+            else
+            {
+                //Get the wall which is closest to both the Player and the Tank which we are going to use to bounce our shot off of
+                WallTarget ClosestWall = GetClosestWall();
+                //Find the location along that wall which is in between the Tank and Players location, then the direction to that location on the wall
+                Vector3 WallTarget = GetWallTarget(ClosestWall);
+                Vector3 ShotDirection = Vector3.Normalize(WallTarget - transform.position);
+                //Spawn the new projectile, store it in the list with the others, then launch it toward that spot on the wall
+                GameObject TankShell = Instantiate(TankShellPrefab, transform.position, Quaternion.identity);
+                ActiveShellProjectiles.Add(TankShell);
+                TankShell.GetComponent<TankShellAI>().InitializeProjectile(ShotDirection, this);
+            }
         }
+    }
+
+    //Returns the position on the wall where the shot should be aimed so it will bounce of and hit the player
+    private Vector3 GetWallTarget(WallTarget Wall)
+    {
+        //First get the players current position to quick access
+        Vector3 PlayerPos = GameState.Instance.Player.transform.position;
+
+        //Measure the half distance between the Player and the Tank on both the X and Y axis
+        float HalfXDistance = Mathf.Abs(PlayerPos.x - transform.position.x) * 0.5f;
+        float HalfYDistance = Mathf.Abs(PlayerPos.y - transform.position.y) * 0.5f;
+
+        //Find which side of the Tank the Player is
+        bool PlayerRightOfTank = PlayerPos.x > transform.position.x;
+        bool PlayerAboveTank = PlayerPos.y > transform.position.y;
+
+        //Now find the position along the target wall directly between the Tank and Player
+        Vector3 WallPos = transform.position;
+        switch(Wall)
+        {
+            case (WallTarget.TopWall):
+                WallPos.x += PlayerRightOfTank ? HalfXDistance : -HalfXDistance;
+                WallPos.y = 4.5f;
+                break;
+            case (WallTarget.BottomWall):
+                WallPos.x += PlayerRightOfTank ? HalfXDistance : -HalfXDistance;
+                WallPos.y = -4.5f;
+                break;
+            case (WallTarget.LeftWall):
+                WallPos.x = -7.5f;
+                WallPos.y += PlayerAboveTank ? HalfYDistance : -HalfYDistance;
+                break;
+            case (WallTarget.RightWall):
+                WallPos.x = 7.5f;
+                WallPos.y += PlayerAboveTank ? HalfYDistance : -HalfYDistance;
+                break;
+        }
+
+        //Return the final target location where the projectile should be aimed to hit the player on a rebound
+        return WallPos;
+    }
+
+    //Figures out which of the 4 walls is closests in relation to both the Tank and the Player
+    private WallTarget GetClosestWall()
+    {
+        Vector3 PlayerPos = GameState.Instance.Player.transform.position;
+
+        //First check the top walls relative distance
+        float TankTopDistance = Mathf.Abs(transform.position.y - 4.5f);
+        float PlayerTopDistance = Mathf.Abs(PlayerPos.y - 4.5f);
+        float RelativeTopDistance = (TankTopDistance + PlayerTopDistance) / 2f;
+
+        //Start by assuming this is the wall that is closest
+        WallTarget ClosestWall = WallTarget.TopWall;
+        float ClosestWallDistance = RelativeTopDistance;
+
+        //Now check the bottom walls relative distance and compare that against the top
+        float TankBottomDistance = Mathf.Abs(transform.position.y - -4.5f);
+        float PlayerBottomDistance = Mathf.Abs(PlayerPos.y - -4.5f);
+        float RelativeBottomDistance = (TankBottomDistance + PlayerBottomDistance) / 2f;
+        if (RelativeBottomDistance < ClosestWallDistance)
+        {
+            ClosestWall = WallTarget.BottomWall;
+            ClosestWallDistance = RelativeBottomDistance;
+        }
+
+        //Now do the same thing with both the Left wall
+        float TankLeftDistance = Mathf.Abs(transform.position.x - -7.5f);
+        float PlayerLeftDistance = Mathf.Abs(PlayerPos.x - -7.5f);
+        float RelativeLeftDistance = (TankLeftDistance + PlayerLeftDistance) / 2f;
+        if (RelativeLeftDistance < ClosestWallDistance)
+        {
+            ClosestWall = WallTarget.LeftWall;
+            ClosestWallDistance = RelativeLeftDistance;
+        }
+
+        //and the Right wall
+        float TankRightDistance = Mathf.Abs(transform.position.x - 7.5f);
+        float PlayerRightDistance = Mathf.Abs(PlayerPos.x - 7.5f);
+        float RelativeRightDistance = (TankRightDistance + PlayerRightDistance) / 2f;
+        if (RelativeRightDistance < ClosestWallDistance)
+        {
+            ClosestWall = WallTarget.RightWall;
+            ClosestWallDistance = RelativeRightDistance;
+        }
+
+        //Finally, return the wall which is going to be used for a rebound shot
+        return ClosestWall;
     }
 
     //Returns a position offset from the players location by some random amount within the allowed value range
